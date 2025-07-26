60f8f86321a497d16cf391b4293990a7ccaf65d5
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-07-26 19:44:42 +0800

    Render HTML pages based on URL path
    - Use Node.js fs and path modules to serve HTML files

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import {createServer} from "node:http";
-
-const server = createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
+const {createServer} = require("node:http");
+const path = require("node:path");
+const fs = require("node:fs/promises")
+const server = createServer(async (req, res) => {
+  let filePath;
+ 
+  if (req.url === "/") {
+    filePath = path.join(__dirname, "project", "index.html");
+    
+  } else if (req.url === "/about") {
+    filePath = path.join(__dirname, "project", "about.html");
+  } else if (req.url === "/contact") {
+    filePath = path.join(__dirname, "project", "contact-me.html");
+  } else {
+    filePath = path.join(__dirname, "project", "404.html")
+  }
+  
+  const data = await fs.readFile(filePath);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(data);
+  
 })
 
 server.listen(8000);
